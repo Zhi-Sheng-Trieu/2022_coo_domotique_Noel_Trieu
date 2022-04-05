7a88f1a4b252c8fae0dd10105e6edf015d6cc900
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-04-05 11:09:18 +0200

    commit

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -3,6 +3,40 @@ import java.util.ArrayList;
 public class Telecommande {
     private ArrayList<Lampe> lampes;
 
+    public Telecommande() {
+        lampes = new ArrayList<Lampe>();
+    }
 
+    public void ajouterLampe(Lampe lampe) {
+        lampes.add(lampe);
+    }
 
+    public void activerLampe(int indiceLampe) {
+        lampes.get(indiceLampe).allumer();
+    }
+
+    public void desactiverLampe(int indiceLampe) {
+        lampes.get(indiceLampe).eteindre();
+    }
+
+    public void activerTout() {
+        for (Lampe lampe : lampes) {
+            lampe.allumer();
+        }
+    }
+
+    public void desactiverTout() {
+        for (Lampe lampe : lampes) {
+            lampe.eteindre();
+        }
+    }
+
+    @Override
+    public String toString() {
+        String res = "La telecommande controle : \n";
+        for (Lampe lampe : lampes) {
+            res += lampe+ "\n";
+        }
+        return res;
+    }
 }
