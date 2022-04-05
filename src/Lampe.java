4e059e6a7e44d0641226afa39662934a665ebbfb
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-04-05 11:18:55 +0200

    commit

--- src/Lampe.java
+++ src/Lampe.java
@@ -1,31 +1,66 @@
+/**
+ * La classe Lampe qui represente une lampe qu'on peut allumer et eteindre.
+ *
+ * elle possede un attribut allume qui rend compte de son etat. cet attribut est
+ * prive et manipulable par les methodes allumer et eteindre
+ */
+
 public class Lampe {
+
+    /**
+     * nom de la lampe
+     */
     private String nom;
+
+    /**
+     * etat de la lampe
+     */
     private boolean allume;
 
-
-    public Lampe(String nom) {
-        this.nom = nom;
+    /**
+     * construit une lampe eteinte par defaut
+     *
+     * @param paramNom
+     *            nom de la lampe
+     */
+    public Lampe(String paramNom) {
         this.allume = false;
+        this.nom = paramNom;
     }
 
-    public String getNom() {
-        return this.nom;
-    }
-
+    /**
+     * permet de changer l'etat interne d'une lampe en l'allumant
+     */
     public void allumer() {
         this.allume = true;
     }
 
+    /**
+     * permet de changer l'etat interne d'une lampe en l'eteignant
+     */
     public void eteindre() {
         this.allume = false;
     }
 
-    public boolean estAllume() {
-        return this.allume;
-    }
-
+    /**
+     * retourne le descriptif de la lampe sous la forme nom + "On"/"Off"
+     */
     public String toString() {
-        return this.nom + " est " + (this.allume ? "allumée" : "éteinte");
+        String r = "";
+        if (this.allume) {
+            r = "On";
+        } else {
+            r = "Off";
+        }
+        return (nom + ": " + r);
     }
 
+    /**
+     * permet de savoir si la lampe est allumee
+     *
+     * @return etat de la lampe
+     */
+    public boolean isAllume() {
+        return allume;
+    }
 }
