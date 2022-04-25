a2c3e2faba42f181679dc0c59e68a5d24711dec8
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-04-25 14:13:48 +0200

    modification de Telecommande

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -2,32 +2,52 @@ import java.util.ArrayList;
 
 public class Telecommande {
     private ArrayList<Lampe> lampes;
+    private ArrayList<Hifi> hifis;
 
     public Telecommande() {
         lampes = new ArrayList<Lampe>();
+        hifis = new ArrayList<Hifi>();
     }
 
     public void ajouterLampe(Lampe lampe) {
         lampes.add(lampe);
     }
 
+    public void ajouterHifi(Hifi hifi) {
+        hifis.add(hifi);
+    }
+
     public void activerLampe(int indiceLampe) {
         lampes.get(indiceLampe).allumer();
+    }
+
+    public void activerHifi(int indiceHifi) {
+        hifis.get(indiceHifi).allumer();
     }
 
     public void desactiverLampe(int indiceLampe) {
         lampes.get(indiceLampe).eteindre();
     }
 
+    public void desactiverHifi(int indiceHifi) {
+        hifis.get(indiceHifi).eteindre();
+    }
+
     public void activerTout() {
         for (Lampe lampe : lampes) {
             lampe.allumer();
+        }
+        for (Hifi hifi : hifis) {
+            hifi.allumer();
         }
     }
 
     public void desactiverTout() {
         for (Lampe lampe : lampes) {
             lampe.eteindre();
+        }
+        for (Hifi hifi : hifis) {
+            hifi.eteindre();
         }
     }
 
